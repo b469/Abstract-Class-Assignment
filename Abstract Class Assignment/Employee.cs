8b407b584309b83d72b7405e9b637ec88091fe9c
luis bolivar <bolivarluis97@yahoo.ca>
2026-03-27 20:51:27 -0700

    Add project files.

---
+++ Abstract Class Assignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    public  class Employee:Person ///Creating another Employee and have it inherit from the Person Class
+   
+    {
+        public int Id { get; set; } // Int Id to Employee
+
+        public override void SayName() // Override the Abstract in Person
+        {
+            Console.WriteLine("Name:" + firsName + "  " + lastName);
+        }
+        
+    
+        
+
+
+    }
+}
